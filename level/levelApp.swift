de44fe0feae11cddc14fbcadc85f0889a8722c94
vleyn <ruuwuu@mail.ru>
2023-06-06 01:59:24 +0300

    initial commit
    add project files

---
+++ level/levelApp.swift
@@ -0,0 +1,17 @@
+//
+//  levelApp.swift
+//  level
+//
+//  Created by Владислав Мазуров on 6.06.23.
+//
+
+import SwiftUI
+
+@main
+struct levelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
